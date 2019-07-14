97e24e7f6773e5f309426abc0d8d2afb4bbea572
user <diesel71@list.ru>
2019-07-14 13:37:40 +0300

    Клиентская часть

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_mes = new System.Windows.Forms.TextBox();
+            this.tb_ip_adr = new System.Windows.Forms.TextBox();
+            this.leb_mes = new System.Windows.Forms.Label();
+            this.leb_IP = new System.Windows.Forms.Label();
+            this.but_sendmes = new System.Windows.Forms.Button();
+            this.tb_hostname = new System.Windows.Forms.TextBox();
+            this.leb_hostname = new System.Windows.Forms.Label();
+            this.ip_servers_list = new System.Windows.Forms.ComboBox();
+            this.leb_ip_serv = new System.Windows.Forms.Label();
+            this.but_ip_server_add = new System.Windows.Forms.Button();
+            this.list_files = new System.Windows.Forms.ListBox();
+            this.lab_list_files = new System.Windows.Forms.Label();
+            this.lab_actions = new System.Windows.Forms.Label();
+            this.actions_list = new System.Windows.Forms.ComboBox();
+            this.but_select_cat = new System.Windows.Forms.Button();
+            this.tb_select_cat = new System.Windows.Forms.TextBox();
+            this.lab_select_cat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_mes
+            // 
+            this.tb_mes.Location = new System.Drawing.Point(86, 48);
+            this.tb_mes.Name = "tb_mes";
+            this.tb_mes.Size = new System.Drawing.Size(100, 20);
+            this.tb_mes.TabIndex = 1;
+            // 
+            // tb_ip_adr
+            // 
+            this.tb_ip_adr.Location = new System.Drawing.Point(431, 33);
+            this.tb_ip_adr.Name = "tb_ip_adr";
+            this.tb_ip_adr.Size = new System.Drawing.Size(100, 20);
+            this.tb_ip_adr.TabIndex = 2;
+            // 
+            // leb_mes
+            // 
+            this.leb_mes.AutoSize = true;
+            this.leb_mes.Location = new System.Drawing.Point(97, 21);
+            this.leb_mes.Name = "leb_mes";
+            this.leb_mes.Size = new System.Drawing.Size(65, 13);
+            this.leb_mes.TabIndex = 3;
+            this.leb_mes.Text = "Сообщение";
+            // 
+            // leb_IP
+            // 
+            this.leb_IP.AutoSize = true;
+            this.leb_IP.Location = new System.Drawing.Point(437, 9);
+            this.leb_IP.Name = "leb_IP";
+            this.leb_IP.Size = new System.Drawing.Size(83, 13);
+            this.leb_IP.TabIndex = 4;
+            this.leb_IP.Text = "IP компьютера";
+            // 
+            // but_sendmes
+            // 
+            this.but_sendmes.Location = new System.Drawing.Point(71, 84);
+            this.but_sendmes.Name = "but_sendmes";
+            this.but_sendmes.Size = new System.Drawing.Size(129, 35);
+            this.but_sendmes.TabIndex = 5;
+            this.but_sendmes.Text = "Отправить сообщение на сервер";
+            this.but_sendmes.UseVisualStyleBackColor = true;
+            this.but_sendmes.Click += new System.EventHandler(this.but_sendmes_Click);
+            // 
+            // tb_hostname
+            // 
+            this.tb_hostname.Location = new System.Drawing.Point(431, 73);
+            this.tb_hostname.Name = "tb_hostname";
+            this.tb_hostname.Size = new System.Drawing.Size(100, 20);
+            this.tb_hostname.TabIndex = 6;
+            // 
+            // leb_hostname
+            // 
+            this.leb_hostname.AutoSize = true;
+            this.leb_hostname.Location = new System.Drawing.Point(454, 55);
+            this.leb_hostname.Name = "leb_hostname";
+            this.leb_hostname.Size = new System.Drawing.Size(34, 13);
+            this.leb_hostname.TabIndex = 7;
+            this.leb_hostname.Text = "Хост ";
+            // 
+            // ip_servers_list
+            // 
+            this.ip_servers_list.FormattingEnabled = true;
+            this.ip_servers_list.Items.AddRange(new object[] {
+            "192.168.1.5",
+            "127.0.0.1"});
+            this.ip_servers_list.Location = new System.Drawing.Point(245, 47);
+            this.ip_servers_list.Name = "ip_servers_list";
+            this.ip_servers_list.Size = new System.Drawing.Size(121, 21);
+            this.ip_servers_list.TabIndex = 8;
+            // 
+            // leb_ip_serv
+            // 
+            this.leb_ip_serv.AutoSize = true;
+            this.leb_ip_serv.Location = new System.Drawing.Point(242, 21);
+            this.leb_ip_serv.Name = "leb_ip_serv";
+            this.leb_ip_serv.Size = new System.Drawing.Size(109, 13);
+            this.leb_ip_serv.TabIndex = 9;
+            this.leb_ip_serv.Text = "Выбрать IP сервера";
+            // 
+            // but_ip_server_add
+            // 
+            this.but_ip_server_add.Location = new System.Drawing.Point(245, 84);
+            this.but_ip_server_add.Name = "but_ip_server_add";
+            this.but_ip_server_add.Size = new System.Drawing.Size(121, 35);
+            this.but_ip_server_add.TabIndex = 10;
+            this.but_ip_server_add.Text = "Добавить IP сервера";
+            this.but_ip_server_add.UseVisualStyleBackColor = true;
+            this.but_ip_server_add.Click += new System.EventHandler(this.but_ip_server_add_Click);
+            // 
+            // list_files
+            // 
+            this.list_files.FormattingEnabled = true;
+            this.list_files.Location = new System.Drawing.Point(71, 154);
+            this.list_files.Name = "list_files";
+            this.list_files.Size = new System.Drawing.Size(198, 186);
+            this.list_files.TabIndex = 11;
+            this.list_files.SelectedIndexChanged += new System.EventHandler(this.list_files_SelectedIndexChanged);
+            // 
+            // lab_list_files
+            // 
+            this.lab_list_files.AutoSize = true;
+            this.lab_list_files.Location = new System.Drawing.Point(83, 140);
+            this.lab_list_files.Name = "lab_list_files";
+            this.lab_list_files.Size = new System.Drawing.Size(148, 13);
+            this.lab_list_files.TabIndex = 12;
+            this.lab_list_files.Text = "Список файлов на сервере:";
+            // 
+            // lab_actions
+            // 
+            this.lab_actions.AutoSize = true;
+            this.lab_actions.Location = new System.Drawing.Point(355, 193);
+            this.lab_actions.Name = "lab_actions";
+            this.lab_actions.Size = new System.Drawing.Size(101, 13);
+            this.lab_actions.TabIndex = 13;
+            this.lab_actions.Text = "Выбрать действие";
+            // 
+            // actions_list
+            // 
+            this.actions_list.FormattingEnabled = true;
+            this.actions_list.Items.AddRange(new object[] {
+            "Получить список файлов",
+            "Отправить на сервер файл",
+            "Загрузить файл с сервера"});
+            this.actions_list.Location = new System.Drawing.Point(358, 209);
+            this.actions_list.Name = "actions_list";
+            this.actions_list.Size = new System.Drawing.Size(162, 21);
+            this.actions_list.TabIndex = 14;
+            this.actions_list.SelectedIndexChanged += new System.EventHandler(this.actions_list_SelectedIndexChanged);
+            // 
+            // but_select_cat
+            // 
+            this.but_select_cat.Location = new System.Drawing.Point(496, 317);
+            this.but_select_cat.Name = "but_select_cat";
+            this.but_select_cat.Size = new System.Drawing.Size(75, 23);
+            this.but_select_cat.TabIndex = 15;
+            this.but_select_cat.Text = "Выбрать";
+            this.but_select_cat.UseVisualStyleBackColor = true;
+            this.but_select_cat.Click += new System.EventHandler(this.but_select_cat_Click);
+            // 
+            // tb_select_cat
+            // 
+            this.tb_select_cat.Location = new System.Drawing.Point(297, 319);
+            this.tb_select_cat.Name = "tb_select_cat";
+            this.tb_select_cat.Size = new System.Drawing.Size(193, 20);
+            this.tb_select_cat.TabIndex = 16;
+            this.tb_select_cat.TextChanged += new System.EventHandler(this.tb_select_cat_TextChanged);
+            // 
+            // lab_select_cat
+            // 
+            this.lab_select_cat.AutoSize = true;
+            this.lab_select_cat.Location = new System.Drawing.Point(311, 303);
+            this.lab_select_cat.Name = "lab_select_cat";
+            this.lab_select_cat.Size = new System.Drawing.Size(131, 13);
+            this.lab_select_cat.TabIndex = 17;
+            this.lab_select_cat.Text = "Каталог для сохранения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 355);
+            this.Controls.Add(this.lab_select_cat);
+            this.Controls.Add(this.tb_select_cat);
+            this.Controls.Add(this.but_select_cat);
+            this.Controls.Add(this.actions_list);
+            this.Controls.Add(this.lab_actions);
+            this.Controls.Add(this.lab_list_files);
+            this.Controls.Add(this.list_files);
+            this.Controls.Add(this.but_ip_server_add);
+            this.Controls.Add(this.leb_ip_serv);
+            this.Controls.Add(this.ip_servers_list);
+            this.Controls.Add(this.leb_hostname);
+            this.Controls.Add(this.tb_hostname);
+            this.Controls.Add(this.but_sendmes);
+            this.Controls.Add(this.leb_IP);
+            this.Controls.Add(this.leb_mes);
+            this.Controls.Add(this.tb_ip_adr);
+            this.Controls.Add(this.tb_mes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+      
+        private System.Windows.Forms.TextBox tb_mes;
+        private System.Windows.Forms.TextBox tb_ip_adr;
+        private System.Windows.Forms.Label leb_mes;
+        private System.Windows.Forms.Label leb_IP;
+        private System.Windows.Forms.Button but_sendmes;
+        private System.Windows.Forms.TextBox tb_hostname;
+        private System.Windows.Forms.Label leb_hostname;
+        private System.Windows.Forms.ComboBox ip_servers_list;
+        private System.Windows.Forms.Label leb_ip_serv;
+        private System.Windows.Forms.Button but_ip_server_add;
+        private System.Windows.Forms.ListBox list_files;
+        private System.Windows.Forms.Label lab_list_files;
+        private System.Windows.Forms.Label lab_actions;
+        private System.Windows.Forms.ComboBox actions_list;
+        private System.Windows.Forms.Button but_select_cat;
+        private System.Windows.Forms.TextBox tb_select_cat;
+        private System.Windows.Forms.Label lab_select_cat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+     
+         int port = 8005; // порт сервера
+        string address;// = "127.0.0.1"; // адрес сервера
+     
+        
+        string fileNameForSending = string.Empty;//хранит имя и путь к файлу который передаётся
+        byte[] data_for_send = new byte[10000];//хранит данные для передачи
+        string working_directory;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            tb_hostname.Text = System.Net.Dns.GetHostName();
+            tb_ip_adr.Text = System.Net.Dns.GetHostByName(System.Net.Dns.GetHostName()).AddressList[0].ToString();
+            working_directory = "filedir";
+            tb_select_cat.Text = "filedir";
+            tb_mes.Text = "";
+
+        }
+        public void sendMessage(string action = null)
+        {
+            address = ip_servers_list.Text;
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                //Console.Write("Введите сообщение:");
+                string message;
+                if (action == null)
+                {
+                    if ((tb_mes.Text == null) || (tb_mes.Text == ""))
+                        message = "<Пустое сообщение>";
+                    else
+                         message = "send_text_message$"+ tb_mes.Text;//Console.ReadLine();
+                    data_for_send = Encoding.Unicode.GetBytes(message);
+                }
+                else
+                {
+                    message = "action$" + action;
+                    data_for_send = Encoding.Unicode.GetBytes(message);
+                }
+                if(action == "get_file")
+                {
+                    string file = list_files.SelectedItem.ToString();
+                    message += "$" +file;
+                    data_for_send = Encoding.Unicode.GetBytes(message);
+                }
+                if(action == "send_file_name")
+                {
+                    // OpenFileDialog openFile = new OpenFileDialog();
+                    string[] list = fileNameForSending.Split('\\');
+                    message += "$" + list[list.Length-1];
+                  //  fileNameForSending = openFile.FileName;
+                    data_for_send = Encoding.Unicode.GetBytes(message);
+                }
+                if (action == "send_file_data")
+                {
+                    data_for_send = File.ReadAllBytes(fileNameForSending);
+                    fileNameForSending = "";
+                }
+                byte[] data = data_for_send;//Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[10000]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                if ((action == null)||(action == "send_file_name")||(action == "send_file_data"))
+                    MessageBox.Show("Ответ сервера - " + builder.ToString());
+                else
+                    parse_message(builder, action, data);
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+          
+        }
+        void send_file()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                //string[] list = openFile.FileName.Split('\\');
+                fileNameForSending = openFile.FileName;
+               // data_for_send = File.ReadAllBytes(openFile.FileName);
+                sendMessage("send_file_name");
+                sendMessage("send_file_data");
+            }
+
+        }
+        private string selected_file()
+        {
+            string f = list_files.SelectedItem.ToString();
+            string[] list = f.Split('\\');
+
+
+
+            return list[list.Length-1];
+        }
+        private void parse_message(StringBuilder answer, string action, byte[] data)
+        {
+            try
+            {
+                switch (action)
+                {
+                    case "get_folders_list":
+                        string content = answer.ToString();
+                        string[] files = content.Split('|');
+                        list_files.Items.Clear();
+                        for (int i = 0; i < files.Length; i++)
+                            list_files.Items.Add(files[i]);
+                        break;
+                    case "get_file":
+                        string file_name = working_directory + '\\' + list_files.SelectedItem.ToString();// selected_file();
+                        FileInfo info;
+                        if (!File.Exists(file_name))
+                        {
+                            info = new FileInfo(file_name);
+                            info.Create().Close();
+
+                        }
+                        File.WriteAllBytes(file_name, data);
+                        MessageBox.Show("Файл с сервера получен");
+                        break;
+
+                }
+            }
+            catch(Exception w) { MessageBox.Show(w.Message); }
+            
+        }
+        private void but_sendmes_Click(object sender, EventArgs e)
+        {
+            // readder = new Thread(reading);
+            //readder.Start();
+            if (tb_mes.Text != null)
+                sendMessage();
+            else
+                MessageBox.Show("Введен пустой текст сообщения");
+        }
+     
+     
+       
+
+        private void but_ip_server_add_Click(object sender, EventArgs e)
+        {
+            string item = Microsoft.VisualBasic.Interaction.InputBox("Введите новый IP");
+            ip_servers_list.Items.Add(item);
+        }
+
+        private void list_files_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void but_select_cat_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.ShowDialog();
+            tb_select_cat.Text = folder.SelectedPath;
+        }
+
+        private void actions_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (actions_list.SelectedIndex == 0)
+                sendMessage("get_folders_list");
+            if(actions_list.SelectedIndex == 1)
+            {
+                send_file();
+            }
+            if(actions_list.SelectedIndex == 2)
+            {
+               
+                  sendMessage("get_file");
+            }
+        }
+
+        private void tb_select_cat_TextChanged(object sender, EventArgs e)
+        {
+            working_directory = tb_select_cat.Text;
+        }
+    }
+}
